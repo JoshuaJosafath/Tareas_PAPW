57384674ad6c95ba73041593989ff1b8cdcf3aae
JoshuaJos <42219773+JoshuaJosafath@users.noreply.github.com>
2018-09-09 22:56:37 -0500

    Update 09/09/18

---
+++ PROYECTO/js/jsDetalle_Articulo.js
@@ -0,0 +1,15 @@
+function Comprar(){
+    var x = document.getElementById("ShowValoration");
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    }else{
+        x.style.display = "block";
+    }
+
+    var x = document.getElementById("ShowToComment");
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    }else{
+        x.style.display = "block";
+    }
+}
